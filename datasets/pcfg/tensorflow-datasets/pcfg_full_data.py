d841abc7f4c8eb640cbc93de50dea720ef48f27d
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2021-07-29 16:21:45 -0700

    Update pcfg_full_data.py

--- datasets/pcfg/tensorflow-datasets/pcfg_full_data.py
+++ datasets/pcfg/tensorflow-datasets/pcfg_full_data.py
@@ -1,8 +1,13 @@
 """pcfg_full_data dataset."""
 
-from example_generator import example_generator
+import sys
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+sys.path.append('../../datasets')
+from datasets.common_files.example_generator import example_generator
+
 
 _DESCRIPTION = """
 Original and iterative decoding data for the standard split of the PCFG dataset.
